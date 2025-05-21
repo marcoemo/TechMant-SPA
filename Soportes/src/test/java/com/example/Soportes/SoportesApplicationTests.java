3cabf0410e179c97aeffd644d6aa48ca85341e54
GiulianoLopresti <gi.lopresti@duocuc.cl>
2025-05-21 06:12:10 -0400

    Create SoportesApplicationTests.java

---
+++ Soportes/src/test/java/com/example/Soportes/SoportesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Soportes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoportesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
