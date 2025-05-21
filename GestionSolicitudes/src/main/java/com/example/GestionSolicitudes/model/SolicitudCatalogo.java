1c606f90926e8d31ae49f0cb1cb6bb065fd855bb
GiulianoLopresti <gi.lopresti@duocuc.cl>
2025-05-21 18:42:42 -0400

    Update SolicitudCatalogo.java

--- GestionSolicitudes/src/main/java/com/example/GestionSolicitudes/model/SolicitudCatalogo.java
+++ GestionSolicitudes/src/main/java/com/example/GestionSolicitudes/model/SolicitudCatalogo.java
@@ -17,9 +17,6 @@ public class SolicitudCatalogo {
     @MapsId("solicitudId")
     @JoinColumn(name = "solicitud_id")
     private Solicitud solicitud;//fk solicitud
-    
-    @Column(name = "Id_catalogo", insertable = false, updatable = false)
-    private Long idCatalogo; //fk servicio
 
     @Column(nullable = false)
     private int subtotal; //atributo de la tabla intermedia
