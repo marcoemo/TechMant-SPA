7aadfd14e53bc9d24802872233bbbd531bb50196
GiulianoLopresti <gi.lopresti@duocuc.cl>
2025-05-21 15:27:53 -0400

    Create SolicitudCatalogo.java

---
+++ GestionSolicitudes/src/main/java/com/example/GestionSolicitudes/model/SolicitudCatalogo.java
@@ -0,0 +1,26 @@
+package com.example.GestionSolicitudes.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "solicitudes_servicios")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SolicitudCatalogo {
+
+    @EmbeddedId
+    private SolicitudCatalogoId id;
+
+    @ManyToOne
+    @MapsId("solicitudId")
+    @JoinColumn(name = "solicitud_id")
+    private Solicitud solicitud;//fk solicitud
+    
+    @Column(name = "servicio_id", insertable = false, updatable = false)
+    private Long servicioId; //fk servicio
+
+    @Column(nullable = false)
+    private int subtotal; //atributo de la tabla intermedia
+}
