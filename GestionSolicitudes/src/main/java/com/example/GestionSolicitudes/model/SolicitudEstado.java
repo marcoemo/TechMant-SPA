66e10a821868b75f477703e1e49b8c13b18ffe59
GiulianoLopresti <gi.lopresti@duocuc.cl>
2025-05-22 18:16:35 -0400

    Update SolicitudEstado.java

--- GestionSolicitudes/src/main/java/com/example/GestionSolicitudes/model/SolicitudEstado.java
+++ GestionSolicitudes/src/main/java/com/example/GestionSolicitudes/model/SolicitudEstado.java
@@ -4,4 +4,4 @@ public enum SolicitudEstado {
     NUEVA, ASIGNADA, EN_DIAGNOSTICO, FINALIZADA, CANCELADA
 }
 
-// en postman (PUT) para cambiar los estados -> solicitudes/{id}/estado?estado=FINALIZADA
+// en postman (PUT) para cambiar los estados -> solicitudes/{id}/estado?estado=FINALIZADA - "nota: la id es sin los {}"
