805493d934e4819808b9d2a75d2cd953d7e2c8a7
GiulianoLopresti <gi.lopresti@duocuc.cl>
2025-05-22 14:39:58 -0400

    Update Reporte.java

--- Reportes/src/main/java/com/example/Reportes/model/Reporte.java
+++ Reportes/src/main/java/com/example/Reportes/model/Reporte.java
@@ -1,5 +1,39 @@
 package com.example.Reportes.model;
 
+import java.time.LocalDate;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "reportes_tecnico")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Reporte {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long reporteId;
+
+    //private Long tecnicoId; // ID del tecnico evaluado
+
+    private Long usuarioId; // Id del usuario que genera el reporte
+
+    @Min(1)
+    @Max(10)
+    @Column(nullable = false)
+    private int calificacion; // del 1 al 10
+
+    @Column(length = 500, nullable = false)
+    private String observaciones;
+
+    @Column(length = 500, nullable = false)
+    private String aspectoDestacado;
+
+    @Column(nullable = false)
+    private LocalDate fechaGeneracion;
 }
