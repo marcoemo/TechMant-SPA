e0042a54f1a0b107a974cb36c52f739984f0fb5a
GiulianoLopresti <gi.lopresti@duocuc.cl>
2025-05-24 06:43:13 -0400

    Update SolicitudService.java

--- GestionSolicitudes/src/main/java/com/example/GestionSolicitudes/service/SolicitudService.java
+++ GestionSolicitudes/src/main/java/com/example/GestionSolicitudes/service/SolicitudService.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
+import com.example.GestionSolicitudes.client.AsignacionClient;
 import com.example.GestionSolicitudes.client.DiagnosticoClient;
 import com.example.GestionSolicitudes.model.Solicitud;
 import com.example.GestionSolicitudes.model.SolicitudEstado;
@@ -17,24 +18,16 @@ public class SolicitudService {
 
     private final SolicitudRepository solicitudRepository;
     private final DiagnosticoClient diagnosticoClient;
+    private final AsignacionClient asignacionClient;
 
-    public SolicitudService(SolicitudRepository solicitudRepository, DiagnosticoClient diagnosticoClient) {
+
+    public SolicitudService(SolicitudRepository solicitudRepository, DiagnosticoClient diagnosticoClient, AsignacionClient asignacionClient) {
         this.solicitudRepository = solicitudRepository;
         this.diagnosticoClient = diagnosticoClient;
+        this.asignacionClient = asignacionClient;
     }
-
-    public Map<String, Object> obtenerDetalleSolicitudConDiagnostico(Long solicitudId) {
-        var solicitud = solicitudRepository.findById(solicitudId).orElse(null);
-        if (solicitud == null) return null;
-
-        Map<String, Object> diagnostico = diagnosticoClient.obtenerDiagnosticoPorId(solicitud.getDiagnosticoId());
-
-        return Map.of(
-            "solicitud", solicitud,
-            "diagnostico", diagnostico
-        );
-    }
-
+    
+    
     //Crear una nueva solicitud con la fecha actual
     public Solicitud crearS(Solicitud solicitud) {
         solicitud.setFechaCreacion(LocalDate.now());
@@ -62,4 +55,31 @@ public class SolicitudService {
     return solicitudRepository.save(solicitud);
     }
 
+
+    
+    //obtener una solicitud con su tecnico REVISAR BIEN
+        public Map<String, Object> obtenerSolicitudConTecnico(Long solicitudId) {
+        Solicitud solicitud = solicitudRepository.findById(solicitudId).orElse(null);
+        if (solicitud == null) return null;
+
+        Map<String, Object> tecnico = asignacionClient.obtenerAsigancionPorId(solicitud.getIdAsignacion());
+
+        return Map.of(
+            "solicitud", solicitud,
+            "tecnico", tecnico
+        );
+        }
+
+    //obtener una solicitud con su diagnostico REVISAR BIEN
+        public Map<String, Object> obtenerDetalleSolicitudConDiagnostico(Long solicitudId) {
+            var solicitud = solicitudRepository.findById(solicitudId).orElse(null);
+            if (solicitud == null) return null;
+
+            Map<String, Object> diagnostico = diagnosticoClient.obtenerDiagnosticoPorId(solicitud.getDiagnosticoId());
+
+            return Map.of(
+                "solicitud", solicitud,
+                "diagnostico", diagnostico
+            );
+        }
 }
