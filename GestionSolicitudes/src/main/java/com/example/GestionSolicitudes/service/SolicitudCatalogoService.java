44767bba8765ee7499c2d90ba76f59abe79d175f
GiulianoLopresti <gi.lopresti@duocuc.cl>
2025-05-24 06:43:15 -0400

    Update SolicitudCatalogoService.java

--- GestionSolicitudes/src/main/java/com/example/GestionSolicitudes/service/SolicitudCatalogoService.java
+++ GestionSolicitudes/src/main/java/com/example/GestionSolicitudes/service/SolicitudCatalogoService.java
@@ -1,10 +1,12 @@
 package com.example.GestionSolicitudes.service;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
+import com.example.GestionSolicitudes.client.CatalogoClient;
 import com.example.GestionSolicitudes.model.Solicitud;
 import com.example.GestionSolicitudes.model.SolicitudCatalogo;
 import com.example.GestionSolicitudes.model.SolicitudCatalogoId;
@@ -15,25 +17,33 @@ import com.example.GestionSolicitudes.repository.SolicitudRepository;
 public class SolicitudCatalogoService {
 
     private final SolicitudCatalogoRepository solCatRepo;
+    private final CatalogoClient catalogoClient;
     private final SolicitudRepository solRepo;
 
-    public SolicitudCatalogoService(SolicitudCatalogoRepository solCatRepo, SolicitudRepository solRepo) {
+    public SolicitudCatalogoService(SolicitudCatalogoRepository solCatRepo, SolicitudRepository solRepo, CatalogoClient catalogoClient) {
         this.solCatRepo = solCatRepo;
         this.solRepo = solRepo;
+        this.catalogoClient = catalogoClient;
     }
 
-    //guardar vinculo entre solicitud y catalogo
     public SolicitudCatalogo crearV(SolicitudCatalogo relacion) {
-        Long solicitudId = relacion.getId().getSolicitudId();
+    Long solicitudId = relacion.getId().getSolicitudId();
+    Long idCatalogo = relacion.getId().getIdCatalogo();
 
-        Solicitud solicitud = solRepo.findById(solicitudId)
-                .orElseThrow(() -> new IllegalArgumentException("Solicitud no encontrada"));
+    // Validar solicitud
+    Solicitud solicitud = solRepo.findById(solicitudId)
+            .orElseThrow(() -> new IllegalArgumentException("Solicitud no encontrada"));
 
-        relacion.setSolicitud(solicitud);
-
-        return solCatRepo.save(relacion);
+    // Validar llamando al microservicio Catalogo
+    Map<String, Object> catalogo = catalogoClient.obtenerCatalogoPorId(idCatalogo);
+    if (catalogo == null || catalogo.isEmpty()) {
+        throw new IllegalArgumentException("Catálogo no encontrado");
     }
 
+    relacion.setSolicitud(solicitud);
+
+    return solCatRepo.save(relacion);
+}
 
     //listar todos los vinculos
     public List<SolicitudCatalogo> obtenerTodos() {
@@ -45,5 +55,4 @@ public class SolicitudCatalogoService {
         return solCatRepo.findById(id);
     }
 
-    //eliminar relacion ??
 }
