1257cd2c6b24b34b1af8ed54a6883b6b39efffc5
GiulianoLopresti <gi.lopresti@duocuc.cl>
2025-05-24 06:43:23 -0400

    Create CatalogoClient.java

---
+++ GestionSolicitudes/src/main/java/com/example/GestionSolicitudes/client/CatalogoClient.java
@@ -0,0 +1,27 @@
+package com.example.GestionSolicitudes.client;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Component
+public class CatalogoClient {
+
+    private final WebClient webClient;
+
+    public CatalogoClient(@Value("${catalogo-service.url}") String catalogoUrl) {
+        this.webClient = WebClient.builder()
+                .baseUrl(catalogoUrl)
+                .build();
+    }
+
+    public Map<String, Object> obtenerCatalogoPorId(Long id) {
+        return this.webClient.get()
+                .uri("/catalogo/{id}", id)
+                .retrieve()
+                .bodyToMono(Map.class)
+                .block();
+    }
+}
